4e6ac9053a6a336903b43847369acfc48b1747e9
Roman <sholokh.r@gmail.com>
2021-07-21 21:18:21 +0300

    Reworked Square class - removed generateColor() method

--- src/main/java/com/romansholokh/randomsetofgeometricshapes/shapes/Square.java
+++ src/main/java/com/romansholokh/randomsetofgeometricshapes/shapes/Square.java
@@ -40,10 +40,6 @@ public class Square implements GeometricShape {
         return Math.pow(getSideLength(), 2);
     }
 
-    private String generateColor() {
-        return ColorGenerator.getRandomColor().toString();
-    }
-
     @Override
     public String toString() {
         return "Shape: square" +
