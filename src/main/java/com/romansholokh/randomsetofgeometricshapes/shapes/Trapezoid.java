97e519854c661826267db1d0c394e7e2f697dbd8
Roman <sholokh.r@gmail.com>
2021-07-22 17:24:15 +0300

    Created Trapezoid class with fields & constructor

---
+++ src/main/java/com/romansholokh/randomsetofgeometricshapes/shapes/Trapezoid.java
@@ -0,0 +1,43 @@
+package com.romansholokh.randomsetofgeometricshapes.shapes;
+
+public class Trapezoid {
+
+    private double baseA;
+    private double baseB;
+    private double legAOrHeight;
+    private double legB;
+    private double midline;
+    private double area;
+    private String color;
+
+    public Trapezoid() {
+    }
+
+    public double getBaseA() {
+        return baseA;
+    }
+
+    public double getBaseB() {
+        return baseB;
+    }
+
+    public double getLegAOrHeight() {
+        return legAOrHeight;
+    }
+
+    public double getLegB() {
+        return legB;
+    }
+
+    public double getMidline() {
+        return midline;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
