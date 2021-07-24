06be551ffa908285c78fdc785fb24e6c56baf5d7
Roman <sholokh.r@gmail.com>
2021-07-24 21:14:44 +0300

    Added REFLECTIONS & CLASSES fields & getter getCLASSES - using Reflections get Set of all inheritors of GeometricShape

--- src/main/java/com/romansholokh/randomsetofgeometricshapes/util/colorgenerator/ShapesetGenerator.java
+++ src/main/java/com/romansholokh/randomsetofgeometricshapes/util/colorgenerator/ShapesetGenerator.java
@@ -1,8 +1,19 @@
 package com.romansholokh.randomsetofgeometricshapes.util.colorgenerator;
 
+import com.romansholokh.randomsetofgeometricshapes.shapes.GeometricShape;
+import org.reflections.Reflections;
+
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ShapesetGenerator {
+
+    private static final Reflections REFLECTIONS = new Reflections("com.romansholokh.randomsetofgeometricshapes.shapes");
+    private static final Set<Class<? extends GeometricShape>> CLASSES = REFLECTIONS.getSubTypesOf(GeometricShape.class);
+
+    private static Set<Class<? extends GeometricShape>> getCLASSES() {
+        return CLASSES;
+    }
 
     private static int generateRandomInt(int min, int max) {
 
